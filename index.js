83bbad4abab479d1a39f5cb458e9e394c9af2c2f
Timo Polvinen <timo.polvinen@gmail.com>
2020-02-27 21:53:11 +0200

    heroku re4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 
 const app = express()
 app.use(cors())
-//app.use(express.static('build'))
+app.use(express.static('build'))
 
 var counter = 0;
 
@@ -38,7 +38,7 @@ app.get('/counter', (req, res) => {
   res.json(info);
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
